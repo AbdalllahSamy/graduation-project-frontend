ba940bac39fcb217537af438f5e811bbd32d7885
ziad4x <zeyad2011am@gmail.com>
2025-04-14 17:45:39 +0200

    colors

--- src/modules/shared/components/AuthLayout/AuthLayout.jsx
+++ src/modules/shared/components/AuthLayout/AuthLayout.jsx
@@ -5,17 +5,17 @@ import auth_pic from '../../../../assets/images/auth-pic.png';
 export default function AuthLayout() {
   return (
     <>
-      <div className="grid md:grid-cols-2  bg-black  w-screen h-screen">
-        <div>
-          <div className="text-white mb-10">
+      <div className="flex  bg-black gap-10  w-screen h-screen ps-10 overflow-auto overflow-y-hidden">
+        <div className='w-1/2'>
+          <div className="text-white mb-10 ">
             {/* <img src={logo} alt="logo" /> */}
           </div>
           <Outlet />
         </div>
 
-        <div className=" ">
-          <div className=" hidden  md:block">
-            <img src={auth_pic} alt="auth-pic" className=" w-screen h-screen" />
+        <div className= "w-1/2">
+          <div className=" hidden   md:flex md:items-start">
+            <img src={auth_pic} alt="auth-pic" className="object-cover max-h-full " />
           </div>
         </div>
       </div>
