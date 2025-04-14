6081e5c0f06e4913d8c4c83107ff789dcd758381
ziad4x <zeyad2011am@gmail.com>
2025-04-14 17:51:49 +0200

    bg

--- src/modules/shared/components/AuthLayout/AuthLayout.jsx
+++ src/modules/shared/components/AuthLayout/AuthLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import auth_pic from '../../../../assets/images/auth-pic.png';
+import auth_pic from '../../../../assets/images/89000b92253dca26e577a35a03740603.jpg';
 // import logo from '../../../../assets/images/Logo.jpg';
 
 export default function AuthLayout() {
   return (
     <>
-      <div className="flex  bg-black gap-10  w-screen h-screen ps-10 overflow-auto overflow-y-hidden">
+      <div className="flex   gap-10  w-screen h-screen ps-10 overflow-auto overflow-y-hidden">
         <div className='w-1/2'>
           <div className="text-white mb-10 ">
             {/* <img src={logo} alt="logo" /> */}
@@ -14,8 +14,8 @@ export default function AuthLayout() {
         </div>
 
         <div className= "w-1/2">
-          <div className=" hidden   md:flex md:items-start">
-            <img src={auth_pic} alt="auth-pic" className="object-cover max-h-full " />
+          <div className=" hidden   md:flex ">
+            <img src={auth_pic} alt="auth-pic" className="object-cover " />
           </div>
         </div>
       </div>
