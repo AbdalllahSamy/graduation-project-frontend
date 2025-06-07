739d591be18f81a5bea7e1e521fed953a4173b56
ZYADNABIL <zizozyad62@gmail.com>
2025-06-07 21:56:45 +0300

    landing-page

--- src/modules/shared/components/MasterLayout/MasterLayout.jsx
+++ src/modules/shared/components/MasterLayout/MasterLayout.jsx
@@ -6,15 +6,9 @@
     return (
       <>
         <div className='flex'>
-<<<<<<< HEAD
           {/* <div>
             <SideBar />
           </div> */}
-=======
-          <div>
-            {/* <SideBar /> */}
-          </div>
->>>>>>> 3b903819f7c45ae6ef64b0c84a5fe0fb4b2a6737
           <div className='w-full '>
             <Outlet />
           </div>
